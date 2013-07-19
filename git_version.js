adc8bff270c7d02ffbc53155646ca6c94ee33c6d
Victor Da Luz <victor@lognllc.com>
2013-07-19 16:38:04 -0600

    Changed to render from external template.

--- git_version.js
+++ git_version.js
@@ -1,23 +1,27 @@
-var git  = require('gift'), repo = git(__dirname), moment = require('moment');
+var git  = require('gift'), moment = require('moment'), _ = require('underscore');
+
+var repo = git(__dirname);
 
 repo.branch(function(err,head){
 
-console.log("BRANCH: " + head.name);
+	var commit_time = moment(head.commit.committed_date);
+
+	var data = {
+		branch : head.name,
+		last_commit : commit_time.calendar()
+	};
 
 
-var commit_time = moment(head.commit.committed_date);
-console.log("LAST COMMIT: " + commit_time.calendar());
+	console.log(data);
 
+	var fs = require('fs');	
 
-var fs = require('fs');
-fs.writeFile(__dirname + "/version.html", "<html><head><title>Version</title></head><body><h1>Branch: " + head.name + "</h1><h2>Last Commit: "+commit_time.calendar()+"</h2></body></html>", function(err) {
-    if(err) {
-        console.log(err);
-    } else {
-        console.log("The version.html file was updated!");
-    }
-}); 
-
-
+	fs.writeFile(__dirname + "/version.html", _.template(fs.readFileSync(__dirname + "/template.html", "utf-8"), data), function(err) {
+	    if(err) {
+	        console.log(err);
+	    } else {
+	        console.log("The version.html file was updated!");
+	    }
+	}); 
 
 });
