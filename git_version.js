b5af6fbdeb1d4139ca01b82fc2f40db24e93af89
Victor Da Luz <victor@lognllc.com>
2013-07-19 18:40:35 -0600

    Get all tags and print commits for each tag

--- git_version.js
+++ git_version.js
@@ -1,29 +1,52 @@
-var git  = require('gift'), moment = require('moment'), _ = require('underscore'), args = process.argv.splice(2), dir = args[0] ? args[0] : __dirname;
+console.log("Fetching data, please wait...");
 
-console.log("git-epic");
+var git  = require('gift'), 
+	moment = require('moment'), 
+	_ = require('underscore'), 
+	args = process.argv.splice(2), dir = args[0] ? args[0] : __dirname,
+	repo = git(dir),
+	data = {tags : []},
+	tagTotal = 0,
+	tagCount = 0,
+	skip = 0;
 
-var repo = git(dir);
-
-repo.branch(function(err,head){
-
-	var commit_time = moment(head.commit.committed_date);
-
-	var data = {
-		branch : head.name,
-		last_commit : commit_time.calendar(),
-		directory : dir
-	};
-
-	console.log(data);
-
+var render = function(){
 	var fs = require('fs');	
 
-	fs.writeFile(__dirname + "/version.html", _.template(fs.readFileSync(__dirname + "/template.html", "utf-8"), data), function(err) {
+	fs.writeFile(dir + "/version.html", _.template(fs.readFileSync(__dirname + "/template.html", "utf-8"), data), function(err) {
 	    if(err) {
 	        console.log(err);
 	    } else {
 	        console.log("The version.html file was updated!");
 	    }
 	}); 
+}
+
+repo.tags(function(err,tags){
+
+	tagTotal = tags.length;
+
+	_.each(tags, function(tag){
+
+		var commit_time = moment(tag.commit.committed_date);
+
+		repo.commits(tag.name, function(err, commits){
+			data.tags.push({
+				name : tag.name,
+				last_commit : commit_time.calendar(),
+				commits : commits
+			});
+
+			tagCount++;
+
+			if(tagCount === tagTotal){
+				render();
+			} 
+		});
+
+
+	});
 
 });
+
+
