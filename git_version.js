eb7b5a4178db0841f60a9a0c5e159fb075b28f54
Victor Da Luz <victor@lognllc.com>
2013-07-23 15:00:10 -0600

    Added support for several command line parameters
    -d: repo directory
    -f: commit message filter
    -o: output file
    -t: template file

--- git_version.js
+++ git_version.js
@@ -1,26 +1,60 @@
 console.log("Fetching data, please wait...");
 
+var parseArgs = function(args){
+
+	parsedArgs = {
+		"dir" : __dirname,
+		"filter" : [],
+		"output" : './version.html',
+		"template" : __dirname + '/template.html'
+	};
+
+	for(var i = 0; i < args.length; i+=2){
+
+		switch(args[i]){
+			case '-d':
+				parsedArgs.dir = args[i+1];
+			break;
+			case '-f':
+				parsedArgs.filter = args[i+1].split(',');
+			break;
+			case '-o':
+				parsedArgs.output = args[i+1];
+			break;
+			case '-t':
+				parsedArgs.template = args[i+1];
+			break;
+
+		}
+
+	}
+	console.log(parsedArgs);
+	return parsedArgs;
+};
+
 var git  = require('gift'), 
 	moment = require('moment'), 
 	_ = require('underscore'), 
-	args = process.argv.splice(2), dir = args[0] ? args[0] : __dirname,
-	repo = git(dir),
+	args = parseArgs(process.argv.splice(2)), 
+	repo = git(args.dir),
 	data = {tags : []},
 	tagTotal = 0,
 	tagCount = 0,
 	skip = 0;
 
+
+
 var render = function(){
 	var fs = require('fs');	
 
-	fs.writeFile(dir + "/version.html", _.template(fs.readFileSync(__dirname + "/template.html", "utf-8"), data), function(err) {
+	fs.writeFile(args.output, _.template(fs.readFileSync(args.template, "utf-8"), data), function(err) {
 	    if(err) {
 	        console.log(err);
 	    } else {
 	        console.log("The version.html file was updated!");
 	    }
 	}); 
-}
+};
 
 repo.tags(function(err,tags){
 
@@ -31,10 +65,29 @@ repo.tags(function(err,tags){
 		var commit_time = moment(tag.commit.committed_date);
 
 		repo.commits(tag.name, function(err, commits){
+			
+			var filteredCommits;
+
+			if(args.filter.length > 0){
+				filteredCommits = _.filter(commits, function(commit){ 
+					var result = false;
+
+					for(var i = 0; i < args.filter.length; i++){
+
+						if(commit.message.lastIndexOf(args.filter[i]) !== -1 ){
+							result = true;
+							break;
+						}
+					}
+
+					return result; 
+				});
+			}
+
 			data.tags.push({
 				name : tag.name,
 				last_commit : commit_time.calendar(),
-				commits : commits
+				commits : args.filter.length > 0 ? filteredCommits : commits
 			});
 
 			tagCount++;
